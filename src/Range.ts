c2af1d0452f063c36d16b04bd0839b4234c7eaf4
yukitomoda <tomoda@rook-jp.com>
2023-07-13 00:30:37 +0900

    feat: Range#contains

--- src/Range.ts
+++ src/Range.ts
@@ -1,4 +1,4 @@
-import { ratio, type Ratio } from 'rational';
+import { type ConvertableToRatio, ratio, Ratio } from 'rational';
 
 class Range {
   /**
@@ -88,6 +88,49 @@ class Range {
       this.left.eq(rhs.left) &&
       this.right.eq(rhs.right)
     );
+  }
+
+  /**
+   * この区間の始点が、指定した点以上の範囲を含むかどうかを調べます。
+   * @param value 判断する基準の点
+   * @param includesValue その点を含むかどうか。falseの場合、valueの点を含まず、それよりも大きい範囲を含むかどうかを判断します。
+   */
+  private leftContains(value: Ratio, includesValue: boolean = true): boolean {
+    if (this.includesLeft || !includesValue) {
+      return this.left.leq(value);
+    } else {
+      return this.left.lt(value);
+    }
+  }
+
+  /**
+   * この区間の終点が、指定した点以下の範囲を含むかどうかを調べます。
+   * @param value 判断する基準の点
+   * @param includesValue その点を含むかどうか。falseの場合、valueの点を含まず、それよりも小さい範囲を含むかどうかを判断します。
+   */
+  private rightContains(value: Ratio, includesValue: boolean = true): boolean {
+    if (this.includesRight || !includesValue) {
+      return this.right.geq(value);
+    } else {
+      return this.right.gt(value);
+    }
+  }
+
+  /**
+   * この区間が指定した値または区間のすべてを含むかどうかを調べます。
+   * @param rhs 調べる値または範囲。
+   */
+  public contains(rhs: Range | ConvertableToRatio): boolean {
+    if (rhs instanceof Range) {
+      if (rhs.isEmpty) return true;
+      if (this.isEmpty) return false;
+      return this.leftContains(rhs.left, rhs.includesLeft) && this.rightContains(rhs.right, rhs.includesRight);
+    } else {
+      if (this.isEmpty) return false;
+      rhs = Ratio.from(rhs);
+      if (rhs.lt(this.left) || this.right.lt(rhs)) return false;
+      return this.leftContains(rhs) && this.rightContains(rhs);
+    }
   }
 }
 
--- tests/Range.ts
+++ tests/Range.ts
@@ -124,4 +124,76 @@ describe('comparison', () => {
     assert.isFalse(Range.open(ratio(-1), ratio(1)).eq(Range.leftHalfOpen(ratio(-1), ratio(1))));
     assert.isFalse(Range.open(ratio(-1), ratio(1)).eq(Range.rightHalfOpen(ratio(-1), ratio(1))));
   });
+
+  describe('contains', () => {
+    it('Ratio', () => {
+      assert.isTrue(Range.point(ratio(0)).contains(ratio(0)));
+      assert.isTrue(Range.closed(ratio(-1), ratio(1)).contains(ratio(1)));
+      assert.isTrue(Range.closed(ratio(-1), ratio(1)).contains(ratio(-1)));
+      assert.isTrue(Range.leftHalfOpen(ratio(-1), ratio(1)).contains(ratio(1)));
+      assert.isTrue(Range.rightHalfOpen(ratio(-1), ratio(1)).contains(ratio(-1)));
+      assert.isTrue(Range.open(ratio(-1), ratio(1)).contains(ratio(0)));
+
+      assert.isFalse(Range.point(ratio(0)).contains(ratio(1)));
+      assert.isFalse(Range.closed(ratio(-1), ratio(1)).contains(ratio(2)));
+      assert.isFalse(Range.closed(ratio(-1), ratio(1)).contains(ratio(-2)));
+      assert.isFalse(Range.leftHalfOpen(ratio(-1), ratio(1)).contains(ratio(-1)));
+      assert.isFalse(Range.rightHalfOpen(ratio(-1), ratio(1)).contains(ratio(1)));
+      assert.isFalse(Range.open(ratio(-1), ratio(1)).contains(ratio(-1)));
+      assert.isFalse(Range.open(ratio(-1), ratio(1)).contains(ratio(1)));
+    });
+
+    describe('Range', () => {
+      it('contains', () => {
+        assert.isTrue(Range.point(ratio(0)).contains(Range.point(ratio(0))));
+        assert.isTrue(Range.empty.contains(Range.empty));
+        assert.isTrue(Range.closed(ratio(-1), ratio(1)).contains(Range.empty));
+        assert.isTrue(Range.closed(ratio(-1), ratio(1)).contains(Range.point(ratio(0))));
+        assert.isTrue(Range.closed(ratio(-1), ratio(1)).contains(Range.closed(ratio(0), ratio(1))));
+        assert.isTrue(Range.closed(ratio(-1), ratio(1)).contains(Range.closed(ratio(-1), ratio(0))));
+        assert.isTrue(Range.closed(ratio(-1), ratio(1)).contains(Range.closed(ratio(-1), ratio(1))));
+        assert.isTrue(Range.closed(ratio(-1), ratio(1)).contains(Range.leftHalfOpen(ratio(-1), ratio(1))));
+        assert.isTrue(Range.closed(ratio(-1), ratio(1)).contains(Range.rightHalfOpen(ratio(-1), ratio(1))));
+        assert.isTrue(Range.closed(ratio(-1), ratio(1)).contains(Range.open(ratio(-1), ratio(1))));
+
+        assert.isTrue(Range.leftHalfOpen(ratio(-1), ratio(1)).contains(Range.leftHalfOpen(ratio(-1), ratio(1))));
+        assert.isTrue(Range.leftHalfOpen(ratio(-1), ratio(1)).contains(Range.closed(ratio(0), ratio(1))));
+        assert.isTrue(Range.leftHalfOpen(ratio(-1), ratio(1)).contains(Range.rightHalfOpen(ratio(-1, 2), ratio(1, 2))));
+        assert.isTrue(Range.leftHalfOpen(ratio(-1), ratio(1)).contains(Range.open(ratio(-1), ratio(1))));
+
+        assert.isTrue(Range.rightHalfOpen(ratio(-1), ratio(1)).contains(Range.rightHalfOpen(ratio(-1), ratio(1))));
+        assert.isTrue(Range.rightHalfOpen(ratio(-1), ratio(1)).contains(Range.closed(ratio(-1), ratio(0))));
+        assert.isTrue(Range.rightHalfOpen(ratio(-1), ratio(1)).contains(Range.leftHalfOpen(ratio(-1, 2), ratio(1, 2))));
+        assert.isTrue(Range.rightHalfOpen(ratio(-1), ratio(1)).contains(Range.open(ratio(-1), ratio(1))));
+
+        assert.isTrue(Range.open(ratio(-1), ratio(1)).contains(Range.open(ratio(-1), ratio(1))));
+        assert.isTrue(Range.open(ratio(-1), ratio(1)).contains(Range.closed(ratio(-1, 2), ratio(1, 2))));
+        assert.isTrue(Range.open(ratio(-1), ratio(1)).contains(Range.leftHalfOpen(ratio(-1, 2), ratio(1, 2))));
+        assert.isTrue(Range.open(ratio(-1), ratio(1)).contains(Range.rightHalfOpen(ratio(-1, 2), ratio(1, 2))));
+      });
+
+      it('not contains', () => {
+        assert.isFalse(Range.empty.contains(Range.point(ratio(0))));
+        assert.isFalse(Range.empty.contains(Range.closed(ratio(-1), ratio(1))));
+        assert.isFalse(Range.point(ratio(0)).contains(Range.closed(ratio(-1), ratio(1))));
+        assert.isFalse(Range.closed(ratio(0), ratio(1)).contains(Range.closed(ratio(-1), ratio(1))));
+        assert.isFalse(Range.closed(ratio(-1), ratio(0)).contains(Range.closed(ratio(-1), ratio(1))));
+        assert.isFalse(Range.leftHalfOpen(ratio(-1), ratio(1)).contains(Range.closed(ratio(-1), ratio(1))));
+        assert.isFalse(Range.rightHalfOpen(ratio(-1), ratio(1)).contains(Range.closed(ratio(-1), ratio(1))));
+        assert.isFalse(Range.open(ratio(-1), ratio(1)).contains(Range.closed(ratio(-1), ratio(1))));
+
+        assert.isFalse(Range.closed(ratio(-1, 2), ratio(1, 2)).contains(Range.leftHalfOpen(ratio(-1), ratio(1))));
+        assert.isFalse(Range.rightHalfOpen(ratio(-1), ratio(1)).contains(Range.leftHalfOpen(ratio(-1), ratio(1))));
+        assert.isFalse(Range.open(ratio(-1), ratio(1)).contains(Range.leftHalfOpen(ratio(-1), ratio(1))));
+
+        assert.isFalse(Range.closed(ratio(-1, 2), ratio(1, 2)).contains(Range.rightHalfOpen(ratio(-1), ratio(1))));
+        assert.isFalse(Range.leftHalfOpen(ratio(-1), ratio(1)).contains(Range.rightHalfOpen(ratio(-1), ratio(1))));
+        assert.isFalse(Range.open(ratio(-1), ratio(1)).contains(Range.rightHalfOpen(ratio(-1), ratio(1))));
+
+        assert.isFalse(Range.closed(ratio(-1, 2), ratio(1, 2)).contains(Range.open(ratio(-1), ratio(1))));
+        assert.isFalse(Range.leftHalfOpen(ratio(-1, 2), ratio(1, 2)).contains(Range.open(ratio(-1), ratio(1))));
+        assert.isFalse(Range.rightHalfOpen(ratio(-1, 2), ratio(1, 2)).contains(Range.open(ratio(-1), ratio(1))));
+      });
+    });
+  });
 });
